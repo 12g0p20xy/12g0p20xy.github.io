c69e56770c4d6304ee8bcac40d84bbaf2b926662
12g0p20xy <12g0p20xy@gmail.com>
2016-03-02 16:25:24 +0800

    test

--- apptest/js/ui.js
+++ apptest/js/ui.js
@@ -1,19 +1,15 @@
 $(function(){
-
-
+  
+  // 瀑布流布局
+  var $grid = $('.grid').imagesLoaded( function() {
+    $grid.masonry();
+  });
 
   $('.brief').on('click', function(event) {
     event.preventDefault();
     var $allItem = $('.brief').removeClass('open');
     $(this).addClass('open');
-    var $grid = $('.grid').imagesLoaded( function() {
-      $grid.masonry();
-    });
+    $grid.masonry();
   });
-
-	// 瀑布流布局
-	var $grid = $('.grid').imagesLoaded( function() {
-	  $grid.masonry();
-	});
 
 })
