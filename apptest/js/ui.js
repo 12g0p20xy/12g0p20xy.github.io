a3c2d3e47298d921aa9d15bfba720c224dfd3417
12g0p20xy <12g0p20xy@gmail.com>
2016-03-02 16:08:09 +0800

    test

--- apptest/js/ui.js
+++ apptest/js/ui.js
@@ -1,28 +1,17 @@
 $(function(){
 
-	var $yuan = $('.yuan img').not('.cover');
-	$yuan.on('click', function(event) {
-		event.preventDefault();
-		chozenCSS = {
-			'animation': 'none',
-			'-webkit-transform': 'translateY(-30px) scale(1.5)',
-			'transform': 'translateY(-30px) scale(1.5)'
-		};
-		$(this).css(chozenCSS);
-		window.setTimeout(
-			function(){ 
-				location.href = "register.html";
-			}, 100);
-	});
 
-	$('.get-reward').on('click', function(event) {
-		event.preventDefault();
-		if (true) {
-			confirm('关注徽商银行宣城分行公众号后即可领取奖品(本地活动仅限宣城微信用户)');
-		}
-		else{
-			alert('成功领取奖品！');
-		}
-	});	
+
+  $('.brief').on('click', function(event) {
+    event.preventDefault();
+    var $allItem = $('.brief').removeClass('open');
+    $(this).addClass('open');
+    $grid.masonry();
+  });
+
+	// 瀑布流布局
+	var $grid = $('.grid').imagesLoaded( function() {
+	  $grid.masonry();
+	});
 
 })
