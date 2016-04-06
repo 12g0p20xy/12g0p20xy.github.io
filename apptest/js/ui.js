8e50cc98ef5f906ddc5bfb1dc22ba4def22b7a33
12g0p20xy <12g0p20xy@gmail.com>
2016-04-06 09:34:40 +0800

    test

---
+++ apptest/js/ui.js
@@ -0,0 +1,28 @@
+$(function(){
+
+	$('.option label').click(function(){
+		var aaa = $(this).closest('li').addClass('chosen').siblings().removeClass('chosen')
+	    .parent().next().addClass('active');
+	});
+
+	//initialize swiper when document ready  
+	var swiper = new Swiper ('.swiper-container', {
+		pagination: '.swiper-pagination',
+		paginationClickable: true,
+		nextButton: '.to-next',
+		prevButton: '.to-prev',
+		paginationType: 'progress'
+	});
+
+	$('.info').on('focus blur', function() {
+		var val1 = $(this).val(),
+			val2 = $(this).siblings().val();
+		if (val1 && val2) {
+			$(this).parent().siblings('.submit-btn').addClass('active');
+		}
+		else{
+			$(this).parent().siblings('.submit-btn').removeClass('active');
+		}
+	});
+
+})
