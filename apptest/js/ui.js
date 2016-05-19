d6982a1c413027f91ae2caca6144c6ace30b1d8c
12g0p20xy <12g0p20xy@gmail.com>
2016-05-19 11:32:23 +0800

    test

--- apptest/js/ui.js
+++ apptest/js/ui.js
@@ -1,30 +1,43 @@
 $(function(){
 
-	$('.option label').click(function(){
-		var aaa = $(this).closest('li').addClass('chosen').siblings().removeClass('chosen')
-	    .parent().next().addClass('active');
-	});
+  // 瀑布流布局
+  var $grid = $('.grid').imagesLoaded( function() {
+    $grid.masonry();
+  });
 
-	//initialize swiper when document ready  
-	var swiper = new Swiper ('.swiper-container', {
-		pagination: '.swiper-pagination',
-		paginationClickable: true,
-		nextButton: '.to-next',
-		prevButton: '.to-prev',
-		paginationType: 'progress'
-	});
+  $('.mask').on('click', function(e) {
+    if (!$(e.target).closest('.register').length) {
+      $('.mask').addClass('hide').removeClass('show');
+    }
+  });
 
-	$('.info').on('focus blur', function() {
-		var val1 = $(this).val(),
-			val2 = $(this).siblings().val();
-		if (val1 && val2) {
-			$(this).parent().siblings('.submit-btn').addClass('active');
-		}
-		else{
-			$(this).parent().siblings('.submit-btn').removeClass('active');
-		}
-		
-		this.scrollIntoView();
-	});
+  $('.btn-3').on('click', function(event) {
+    event.preventDefault();
+    $('.mask').addClass('show').removeClass('hide');
+  });
 
-})
+  $('.delete').on('click', function() {
+    $(this).parent().animate({opacity: 0}, 'fast',
+      function(){
+        $(this).remove();
+      });
+  });
+
+  $('.vote-btn').on('click', function(e) {
+    e.preventDefault();
+    if (!$(this).hasClass('voted')) {
+      $(this).val('已投票').addClass('voted');
+    }
+  });
+
+  // //initialize swiper when document ready  
+  // var swiper = new Swiper ('.swiper-container', {
+  //   pagination: '.swiper-pagination',
+  //   paginationClickable: true,
+  //   nextButton: '.swiper-button-next',
+  //   prevButton: '.swiper-button-prev'
+  // });
+
+  // $('.swiper-pagination-bullet').eq(1).trigger('click');
+
+});
