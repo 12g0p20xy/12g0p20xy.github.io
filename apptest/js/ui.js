0dab12877244580a5d8923bd58fa7b68c14ef02a
12g0p20xy <12g0p20xy@gmail.com>
2016-06-17 16:06:25 +0800

    test

--- apptest/js/ui.js
+++ apptest/js/ui.js
@@ -1,72 +1,34 @@
 $(function(){
 
-	var pocket =0;
-	var removepackage = setInterval(function(){
-				for(var jj=0;jj<$('.div>div').size()/4;jj++){
-					$('.div>div').eq($('.div>div').size()-jj).remove();
-				}
-			},1000);
-	var t = 0;
+	// $(window).scroll(function() {
+	// 	var scroll = $(window).scrollTop();
+	// 	$(".user-bg img").css({
+	// 		width: (100 + scroll/5)  + "%",
+	// 		top: -(scroll/10)  + "%",
+	// 		//Blur suggestion from @janwagner: http://codepen.io/janwagner/ in comments
+	// 		"-webkit-filter": "blur(" + (scroll/200) + "px)",
+	// 		filter: "blur(" + (scroll/200) + "px)"
+	// 	});
+	// });
 
-	function rain(){
-		t++;
-		for(var i=0;i<4;i++){
-			var m=parseInt(Math.random()*700+100);
-			var j2=parseInt(Math.random()*300+1200);
-			var j=parseInt(Math.random()*1600+000);
-			var j1=0;
-			var n=parseInt(Math.random()*10+(-10));
-			$('.div').prepend('<div class="dd"></div>');
-			$('.div').children('div').eq(0).css({'left':j,'top':n});
-			$('.div').children('div').eq(0).animate({'left':j-j1,'top':$(window).height()+20},3000);
-		}
-		timer = setTimeout(rain, 200);
-		// if (t >= 30) {
-		// 	$(".result-box").show();
-		// 	clearTimeout(timer, 20);
-		// }
-	}
+	// $('header').on('touchmove', function(e) {
+	// 	e.preventDefault();
+	// 	var $bg = $(this).find('.user-bg'),
+	// 	    touchY = e.touches[0].clientY;
+	// 	console.log(touchY);
+	// });
 
-	$(document).on('touchend', '.start-game', function(event) {
-		event.preventDefault();
-		setTimeout(rain, 200);
-		$(this).addClass('hide');
-		$(".div").addClass("bg_1");
+
+	var $header = $('header');
+	var myHammer = new Hammer($header.get(0)); //转换成原生DOM
+	myHammer.get("pan").set({
+	    // 默认是禁用纵向移动的，用下面这行代码开启
+	    direction: Hammer.DIRECTION_ALL
 	});
-
-	$(document).on('touchend', '.dd', function(event){
-		event.stopPropagation();
-		$(this).css("background-position","0 -100px");
-		pocket++;
-		if(pocket == 5){
-			$(".result").addClass('show');
-			clearTimeout(timer, 20);
-			setTimeout(function(){
-				$('.result .btn').attr('href', 'register.html');
-				$('.result .more').attr('href', '');
-			}, 2000);
-		}
-	});
-
-	var $weixin = $('.weixin').on('touchend', function(event) {
-		event.preventDefault();
-		$(this).siblings('.qr')
-			.animate({
-				width: 60 + "%",
-				right: 20 + '%',
-				bottom: 200 + '%'
-			}, 200);
-	});
-
-	$(document).on('touchend', function() {
-		if(!$(event.target).closest('nav').length){
-			$weixin.siblings('.qr')
-			.animate({
-				width: 0,
-				right: 0,
-				bottom: 0
-			}, 200);
-		} 
+	myHammer.on('pan', function(e) {
+		console.log(e.deltaY);
+		var headerH = $header.height();
+		$header.height(headerH + e.deltaY/100);
 	});
 
 });
