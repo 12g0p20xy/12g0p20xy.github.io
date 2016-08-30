2436c9c1450feb1b5d2a4378628b96c074339ea1
12g0p20xy <12g0p20xy@gmail.com>
2016-08-30 16:59:23 +0800

    test

--- apptest/js/ui.js
+++ apptest/js/ui.js
@@ -1,20 +1,35 @@
 $(function(){
 
-    var mySwiper = new Swiper ('.swiper-container', {
-        loop : true,
-        pagination: '.swiper-pagination'
-        // nextButton: '.swiper-button-next',
-        // prevButton: '.swiper-button-prev'
-    });
+	function GetRTime(){
+	    var EndTime= new Date('2016/09/12 07:05:00');
+	    var NowTime = new Date();
+	    var t =EndTime.getTime() - NowTime.getTime();
+	    var d=0;
+	    var h=0;
+	    var m=0;
+	    var s=0;
+	    if(t>=0){
+	      d=Math.floor(t/1000/60/60/24);
+	      h=Math.floor(t/1000/60/60%24);
+	      m=Math.floor(t/1000/60%60);
+	      s=Math.floor(t/1000%60);
+	    }
+	    document.getElementById("t_d").innerHTML = d;
+	    document.getElementById("t_h").innerHTML = h;
+	    document.getElementById("t_m").innerHTML = m;
+	    document.getElementById("t_s").innerHTML = s;
+	}
+	setInterval(GetRTime,0);
 
-    var $submitBtn = $('.submit-btn');
-    $submitBtn.one('click', function() {
-        $(this).addClass('active').html('已投票' + '<i>+1</i>');
-    });
+	var $sBar = $('.searchbar'),
+		sDis = $sBar.offset().top;
+	$(document).on('scroll',  function() {
+		if (document.body.scrollTop >= sDis) {
+			$sBar.addClass('fixed');
+		}
+		else if (document.body.scrollTop < sDis) {
+			$sBar.removeClass('fixed');
+		}
+	});
 
-    var $hintBox = $('.hint-box');
-    setTimeout(function(){
-        $hintBox.css('display', 'none');
-    }, 2400);
-    
 });
