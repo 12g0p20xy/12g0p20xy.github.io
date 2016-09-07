c3633f0aaa28bc126ec5c7a505beef7d31de79c0
12g0p20xy <12g0p20xy@gmail.com>
2016-09-07 11:37:40 +0800

    test

--- apptest/js/ui.js
+++ apptest/js/ui.js
@@ -1,35 +1,47 @@
 $(function(){
 
-	function GetRTime(){
-	    var EndTime= new Date('2016/09/12 07:05:00');
-	    var NowTime = new Date();
-	    var t =EndTime.getTime() - NowTime.getTime();
-	    var d=0;
-	    var h=0;
-	    var m=0;
-	    var s=0;
-	    if(t>=0){
-	      d=Math.floor(t/1000/60/60/24);
-	      h=Math.floor(t/1000/60/60%24);
-	      m=Math.floor(t/1000/60%60);
-	      s=Math.floor(t/1000%60);
-	    }
-	    document.getElementById("t_d").innerHTML = d;
-	    document.getElementById("t_h").innerHTML = h;
-	    document.getElementById("t_m").innerHTML = m;
-	    document.getElementById("t_s").innerHTML = s;
-	}
-	setInterval(GetRTime,0);
+	var login = false;
 
-	var $sBar = $('.searchbar'),
-		sDis = $sBar.offset().top;
-	$(document).on('scroll',  function() {
-		if (document.body.scrollTop >= sDis) {
-			$sBar.addClass('fixed');
+	// 投票按钮和弹出框
+	var $showBtn = $('.vote-btn'),
+		$layer = $('.layer'),
+		$closeBtn = $layer.find('.close-btn');
+
+	$showBtn.on('touchstart', function() {
+		if (login && !$(this).hasClass('voted')) {
+			// alert('每天只能投3票');
+			$(this).addClass('voted').html('已投票' + '<i>+1</i>');
 		}
-		else if (document.body.scrollTop < sDis) {
-			$sBar.removeClass('fixed');
+		else if ($(this).hasClass('voted')) {
+			alert('每人只能投一票哦～');
+		}
+		else if (!login){
+			$layer.addClass('show');
+			$('body').css('overflow', 'hidden');
+			$(document).on('touchstart', function() {
+				if (event.target == $layer[0]) {
+					$layer.removeClass('show');
+					$('body').css('overflow', 'visible');
+				}
+			});
+			$closeBtn.on('touchstart', function() {
+				$layer.removeClass('show');
+				$('body').css('overflow', 'visible');
+			});
 		}
 	});
+	
+	
+
+	// var $sBar = $('.searchbar'),
+	// 	sDis = $sBar.offset().top;
+	// $(document).on('scroll',  function() {
+	// 	if (document.body.scrollTop >= sDis) {
+	// 		$sBar.addClass('fixed');
+	// 	}
+	// 	else if (document.body.scrollTop < sDis) {
+	// 		$sBar.removeClass('fixed');
+	// 	}
+	// });
 
 });
