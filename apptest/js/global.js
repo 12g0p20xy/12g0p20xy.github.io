e031c752f39753bf5f5c9a9b090fc0472f64359e
12g0p20xy <12g0p20xy@gmail.com>
2016-09-29 11:32:06 +0800

    test

--- apptest/js/global.js
+++ apptest/js/global.js
@@ -1,1 +1,49 @@
-/*! grunt_test - v1.0.1 - 2016-09-22 */$(function(){var a=$(".grid-item");console.log("global.js更新成功, 图片数目是"+a.length)});
+;(function($){
+
+    var	$gridItem = $('.grid-item'),
+    	$checkBtn = $gridItem.find('.check-btn');
+    $checkBtn.on('click', function() {
+    	if ($(this).hasClass('active')) {
+    		$(this).removeClass('active').html("选 择");
+    	}
+    	else{
+    		$(this).addClass('active').html("<i></i>已选择");
+    	}
+    });
+
+    // 弹出框
+    var $layer = $('.layer'),
+		$closeBtn = $layer.find('.close-btn');
+		
+	var $voteBtn = $('.vote-btn');
+	$voteBtn.on('click', function(event) {
+		event.preventDefault();
+		$.modalShow();
+	});
+
+	var $confirmBtn = $layer.find('.confirm-btn').on('click',function(event) {
+		event.preventDefault();
+		window.location.href = "share.html";
+	});
+
+    $.modalShow = function(){
+		$layer.addClass('show');
+		$('main, section, nav').addClass('blur');
+		$('body').css('overflow', 'hidden');
+		$(document).on('click', function(event) {
+			if (event.target == $layer[0]) {
+				$.modalHide();
+			}
+		});
+		$closeBtn.on('click', function(){
+			$.modalHide();
+		});
+	}
+
+	$.modalHide = function(){
+		$layer.removeClass('show');
+		$('main, section, nav').removeClass('blur');
+		$('body').css('overflow', 'visible');
+	}
+
+})(jQuery);
