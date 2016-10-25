2fd897db3a1341c788960b5c16d6522ba0dcae5f
monad <12g0p20xy@gmail.com>
2016-10-25 19:53:58 +0800

    test

--- apptest/js/shake.js
+++ apptest/js/shake.js
@@ -13,6 +13,36 @@ var Shake = (function() {
 		else {
 			alert('本设备不支持devicemotion事件');
 		}
+
+		// 摇动检测
+		function deviceMotionHandler(eventData) {
+
+			var SHAKE_THRESHOLD = 3000, // 触发摇动的最小距离
+				last_update = 0, // 最后更新的时间
+				x = y = z = last_x = last_y = last_z = 0; // 移动距离
+
+			var acceleration = eventData.accelerationIncludingGravity,
+				curTime = new Date().getTime(),
+				speed = 0;
+
+			if ((curTime - last_update) > 100) {
+				var diffTime = curTime - last_update;
+				last_update = curTime;
+				x = acceleration.x;
+				y = acceleration.y;
+				z = acceleration.z;
+				speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000;
+
+				if (speed > SHAKE_THRESHOLD) {
+					count++;
+					oCount.innerHTML = count;
+				}
+				last_x = x;
+				last_y = y;
+				last_z = z;
+			}
+
+		}
 	}
 
 	// 移除绑定事件
@@ -20,35 +50,7 @@ var Shake = (function() {
 		window.removeEventListener('devicemotion', deviceMotionHandler, false);
 	}
 
-	// 摇动检测
-	function deviceMotionHandler(eventData) {
-
-		var SHAKE_THRESHOLD = 3000, // 触发摇动的最小距离
-			last_update = 0, // 最后更新的时间
-			x = y = z = last_x = last_y = last_z = 0; // 移动距离
-
-		var acceleration = eventData.accelerationIncludingGravity,
-			curTime = new Date().getTime(),
-			speed = 0;
-
-		if ((curTime - last_update) > 100) {
-			var diffTime = curTime - last_update;
-			last_update = curTime;
-			x = acceleration.x;
-			y = acceleration.y;
-			z = acceleration.z;
-			speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000;
-
-			if (speed > SHAKE_THRESHOLD) {
-				count++;
-				oCount.innerHTML = count;
-			}
-			last_x = x;
-			last_y = y;
-			last_z = z;
-		}
-
-	}
+	
 	
 	return {
 		init: function() {
